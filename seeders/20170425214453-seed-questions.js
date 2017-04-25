32a6200a1f8b0f426c9c66757775ebd932aa1390
cyndizhao <cyndi.zhao.sfu@gmail.com>
2017-04-25 15:21:57 -0700

    Seed Questions & Install faker

---
+++ seeders/20170425214453-seed-questions.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// require model
+const M = require('../models/index');
+//
+const Question = M.Question;
+
+const faker = require('faker');
+
+// ð Array.from can create arrays in a variety of different ways
+// using it as follows will create an array with 100 undefined elements
+const questions = Array
+  .from({length: 100})
+  .map(function(){
+    return Question.create({title:`${faker.hacker.adjective()} ${faker.hacker.noun()}`, description:faker.hacker.phrase()})
+  })
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    // the up & down methods need to return a promise
+    // otherwise the command `sequelize db:seed:all` will not work
+    return Promise.all(questions);
+    // All Sequelize model methods return promises
+
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
