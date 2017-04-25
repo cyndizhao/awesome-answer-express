512bc3885f7846cb03f0f8a60e090bd21ed84c44
cyndizhao <cyndi.zhao.sfu@gmail.com>
2017-04-25 13:35:42 -0700

    Install nodemon

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+//four middleware
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
