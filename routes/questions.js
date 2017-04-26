de3ac5bddb243b753a6a10fcd464c2cc7fdd64c5
cyndizhao <cyndi.zhao.sfu@gmail.com>
2017-04-26 13:51:01 -0700

    Add question#new and question#create

--- routes/questions.js
+++ routes/questions.js
@@ -8,13 +8,33 @@ const Question = require('../models/index').Question;
 // Questions#index URL: /questions VERB: GET
 router.get('/', function(request, response, next){
   Question
-  .findAll() //this return a promise allowing us to user .then
+  .findAll({order: [['createdAt', 'DESC'], ['updatedAt', 'DESC']]}) //this return a promise allowing us to user .then
   .then(function(questions){
     //!!!!!!the path of the template that response.render takes is relative to the view/ folder by default
     response.render('questions/index', {questions: questions});
     // the second argument passed to response.render is an object where all its properties will be availble to the rendered template as variables
   })
   // response.send('Stuff');
+})
+
+//Questions#new URL: /questions/new VERB: GET
+router.get('/new', function(req, res){
+  const question = Question.build();
+  res.render('questions/new', {question: question});
+})
+router.post('/', function(req, res){
+  // .body is a property of the request object that
+  // contains all form data as a JavaScript object
+  // res.send(req.body);
+
+  // const title = req.body.title;
+  // const description = req.body.description;
+  const {title, description} = req.body;
+  Question.create({title: title, description: description})
+  .then(function(question){
+    res.redirect('/questions');
+  })
+
 })
 
 // Questions#show URL: /questions/:id VERB: GET
@@ -26,5 +46,8 @@ router.get('/:id', function(req, res){
   });
 
 })
+
+
+
 
 module.exports = router;
