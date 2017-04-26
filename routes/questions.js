db336c2ede0f6f2b20ea91558033df0c9dcc5063
cyndizhao <cyndi.zhao.sfu@gmail.com>
2017-04-25 22:00:55 -0700

    Display all questions

--- routes/questions.js
+++ routes/questions.js
@@ -9,7 +9,9 @@ router.get('/', function(request, response, next){
   Question
   .findAll() //this return a promise allowing us to user .then
   .then(function(questions){
-    response.send(questions);
+    //!!!!!!the path of the template that response.render takes is relative to the view/ folder by default
+    response.render('questions/index', {questions: questions});
+    // the second argument passed to response.render is an object where all its properties will be availble to the rendered template as variables
   })
   // response.send('Stuff');
 })
